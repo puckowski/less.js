d698ac796feb39f352c3c6d5c9b425f9a49a8388
Daniel Puckowski <puckowski.d@gmail.com>
2024-11-29 13:14:25 -0500

    fix(issue:3737) allow blank variable declarations
    * Fixes issue with blank CSS variable declarations.

--- packages/less/src/less/parser/parser.js
+++ packages/less/src/less/parser/parser.js
@@ -5,6 +5,7 @@ import getParserInput from './parser-input';
 import * as utils from '../utils';
 import functionRegistry from '../functions/function-registry';
 import { ContainerSyntaxOptions, MediaSyntaxOptions } from '../tree/atrule-syntax';
+import Anonymous from '../tree/anonymous';
 
 //
 // less.js - parser
@@ -1578,7 +1579,11 @@ const Parser = function Parser(context, imports, fileInfo, currentIndex) {
 
                         // Custom property values get permissive parsing
                         if (name[0].value && name[0].value.slice(0, 2) === '--') {
-                            value = this.permissiveValue(/[;}]/);
+                            if (parserInput.$char(';')) {
+                                value = new Anonymous('');
+                            } else {
+                                value = this.permissiveValue(/[;}]/);
+                            }
                         }
                         // Try to store values as anonymous
                         // If we need the value later we'll re-parse it in ruleset.parseValue
